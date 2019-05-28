f11eb8873eb29dba6356bbd4eef6fe043373f528
Philipczyk, Pawel <pawel.philipczyk@sap.com>
2019-05-28 08:34:48 +0200

    Add is-loading state handling for image gallery

--- js/gallery.js
+++ js/gallery.js
@@ -9,17 +9,27 @@ function gallery() {
 
   function onClick(e) {
     e.preventDefault();
-    const target = e.currentTarget;
-    if(target.href) {
-      // Set active class
-      document.querySelectorAll(GALLERY_THUMBNAILS).forEach(elem => {elem.classList.remove(ACTIVE_CLASS)});
-      target.classList.add(ACTIVE_CLASS);
+    const trigger = e.currentTarget;
+    if (trigger.href && !trigger.classList.contains('is-active')) {
+      // Set loading and active classes
+      document.querySelector(GALLERY_MAIN).classList.add('is-loading');
+      document.querySelectorAll(GALLERY_THUMBNAILS).forEach(elem => {
+        elem.classList.remove(ACTIVE_CLASS);
+      });
+      trigger.classList.add(ACTIVE_CLASS);
       // Update main image
-      document.querySelector(GALLERY_MAIN).src = target.href
+      document.querySelector(GALLERY_MAIN).src = trigger.href;
     }
   }
 
   function init() {
+    document
+      .querySelectorAll(GALLERY_MAIN)
+      .forEach(elem =>
+        elem.addEventListener('load', e =>
+          e.target.classList.remove('is-loading'),
+        ),
+      );
     document.querySelectorAll(GALLERY_THUMBNAILS).forEach(elem => {
       elem.addEventListener('click', onClick);
     });
